bdc11547f5eb3bf8c45674d0d66fbcce2ea2b212
Taeyeon Mori <orochimarufan.x3@gmail.com>
2015-09-01 15:21:06 +0000

    Accumulated changes

--- share/scripts/cinit-shared.sh
+++ share/scripts/cinit-shared.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/sh -e
 # (c) 2015 Taeyeon Mori
 # Set up shared mount
-# Add to pre-start
+# Add to hook.mount
 
 SHARED=/run/lxc/shared
-test -e $SHARED || lxc-shared-init
-test -e $SHARED/$LXC_NAME || mkdir $SHARED/$LXC_NAME
+MOUNTPOINT=/shared
+
+# Check if we need to set up the shared directory
+if ! cat /proc/mounts | cut -d" " -f2 | grep -q "$SHARED"; then
+    [ -d "$SHARED" ] || mkdir -p "$SHARED"
+    mount -t tmpfs shared "$SHARED"
+    mount --make-unbindable "$SHARED"
+    mount --make-shared "$SHARED"
+fi
+
+# Mount the shared dir
+[ -d "$SHARED/$LXC_NAME" ] || mkdir "$SHARED/$LXC_NAME"
+[ -d "$LXC_ROOTFS_MOUNT$MOUNTPOINT" ] || mkdir "$LXC_ROOTFS_MOUNT$MOUNTPOINT"
+mount --bind "$SHARED/$LXC_NAME" "$LXC_ROOTFS_MOUNT$MOUNTPOINT"
 
